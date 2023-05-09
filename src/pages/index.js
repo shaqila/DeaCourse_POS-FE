03e1afaebe016e201855e467294887655b454573
Shaqila <shaqila.erbeliza@outlook.com>
2023-05-09 16:52:07 +0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import Layout from "@/components/layouts/Layout";
+import api from "@/api";
+import ProductList from "@/components/elements/ProductList/ProductList";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProduct = async () => {
+    const response = await api.get("/products");
+    const data = await response.data.payload;
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  return (
+    <Layout>
+      <h1>HOME</h1>
+      <div className={styles.home}>
+        <ProductList products={products} />
+      </div>
+    </Layout>
+  );
+}
---
+++ src/pages/transaction.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/layouts/Layout";
+
+export default function Transaction() {
+  return (
+    <Layout>
+      <p>Transaction</p>
+    </Layout>
+  );
+}
